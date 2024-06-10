b0dcd6cba0b295a216aea02d4829c429e3ccf7e1
kAly <kvnclt.pro@gmail.com>
2024-06-10 17:16:47 +0200

    update

---
+++ 19- Initiation  Scripting Windows PowerShell/script3.ps1
@@ -0,0 +1,27 @@
+﻿function script_menu {
+    
+    do {
+
+    Write-Output "MENU :"
+    Write-Output "1. Informations Gérérales"
+    Write-Output "2. Informations Lecteurs"
+    Write-Output "3. Informations Réseaux"
+    Write-Output "4. Quitter"
+
+    $my_var = Read-Host 'Entrez votre nombre'
+
+    if ($my_var -eq '1') {
+        Get-ComputerInfo -Property CsCaption, CsDomain, Osname, OsArchitecture, OssystemDirectory
+    }
+    elseif ($my_var -eq '2') {
+        Get-WmiObject -Class Win32_LogicalDisk 
+    }
+    elseif ($my_var -eq '3') {
+        Get-NetAdapter
+    }
+
+  } while ($my_var -ne '4')
+
+}
+
+script_menu
